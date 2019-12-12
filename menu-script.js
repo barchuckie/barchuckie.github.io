3db6d5f42e2039938483d2ba05402948e69357d8
barchuckie <barchuckie@outlook.com>
2019-12-12 11:44:36 +0100

    small update & removed jquery

--- menu-script.js
+++ menu-script.js
@@ -1,12 +1,13 @@
 function controlNavbar() {
-    var x = document.getElementById("main-nav");
-    if (x.classList.contains("responsive")) {
-        x.classList.remove("responsive");
+    var navigationBar = document.getElementById("main-nav");
+    if (navigationBar.classList.contains("responsive")) {
+        navigationBar.classList.remove("responsive");
     } else {
-        x.classList.add("responsive");
+        navigationBar.classList.add("responsive");
     }
 }
 
-$(document).ready(function () {
-   $('#main-nav').css('display', 'flex');
-});
+function ready() {
+    var navigationBar = document.getElementById("main-nav");
+    navigationBar.style.display = 'flex';
+}
